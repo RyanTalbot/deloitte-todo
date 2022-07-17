9c81616e55513b4ff806daef4d70dbcbc36e9b00
Ryan Talbot <ryan-talbot@outlook.com>
2022-07-17 20:47:01 +0100

    add GET mappings

---
+++ src/main/java/deloittetodo/controllers/TodoFormController.java
@@ -0,0 +1,42 @@
+package deloittetodo.controllers;
+
+import deloittetodo.models.TodoElement;
+import deloittetodo.repositories.TodoElementRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+public class TodoFormController {
+    private final Logger logger = LoggerFactory.getLogger(TodoFormController.class);
+
+    @Autowired
+    private TodoElementRepository todoElementRepository;
+
+    @GetMapping("/create-todo")
+    public String showCreateForm(TodoElement todoElement) {
+        return "add-todo";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+        TodoElement todoElement = todoElementRepository
+                .findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("TodoElement id: " + id + " not found"));
+
+        model.addAttribute("todo", todoElement);
+        return "update-todo";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteToDoItem(@PathVariable("id") long id, Model model) {
+        TodoElement todoElement = todoElementRepository
+                .findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("TodoItem id: " + id + " not found"));
+
+        todoElementRepository.delete(todoElement);
+        return "redirect:/";
+    }
+}
