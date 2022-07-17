a3d2275953b91173d2eef9da4bb77aa42cb632a1
Ryan Talbot <ryan-talbot@outlook.com>
2022-07-17 13:25:05 +0100

    init repo

---
+++ src/main/java/deloittetodo/DeloitteTodoApplication.java
@@ -0,0 +1,13 @@
+package deloittetodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeloitteTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeloitteTodoApplication.class, args);
+	}
+
+}
